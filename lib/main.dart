4fa2b3c5e924027212159910d448f5a5403a917f
Hadeel-AL-Talli <hadeeltali7@gmail.com>
2021-11-14 21:54:35 -0800

    provider commit3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:valuet_space_app/database/db_controller.dart';
+import 'package:valuet_space_app/provider/passwords_provider.dart';
 import 'package:valuet_space_app/screens/launch_screen.dart';
 import 'package:valuet_space_app/screens/on_boarding_screen.dart';
 import 'package:valuet_space_app/screens/passwordsscreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DbController().initDatabase();
   runApp(const MyApp());
 }
 
@@ -18,26 +23,31 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      initialRoute: '/launch_screen',
-      routes:{
-        '/launch_screen':(context) => LaunchScreen(),
-       '/on_boarding_screen':(context)=> OnBoardingScreen(),
-        '/passwords_sreen' : (context) => PasswordsScreen(),
-      // '/addPassword_screen': (context) => AddPasswordScreen(),
-      //'about_screen' : (context) => AboutScreen(),
-
-
-      },
-      localizationsDelegates: [
-       GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
+    return  MultiProvider(
+      providers: [
+        ChangeNotifierProvider<PasswordsProvider>(create: (_)=> PasswordsProvider())
       ],
-     
-      
-
-      
+      child: MaterialApp(
+        initialRoute: '/launch_screen',
+        routes:{
+          '/launch_screen':(context) => LaunchScreen(),
+         '/on_boarding_screen':(context)=> OnBoardingScreen(),
+          '/passwords_sreen' : (context) => PasswordsScreen(),
+        // '/addPassword_screen': (context) => AddPasswordScreen(),
+        //'about_screen' : (context) => AboutScreen(),
+    
+    
+        },
+        localizationsDelegates: [
+         GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+       
+        
+    
+        
+      ),
     );
   }
 }
---
+++ lib/provider/passwords_provider.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:valuet_space_app/database/controllers/password_db_controller.dart';
+import 'package:valuet_space_app/models/passwordmodel.dart';
+
+class PasswordsProvider extends ChangeNotifier {
+  List<Passwords> passwords = <Passwords>[];
+
+  final PasswordDBController _passwordDBController = PasswordDBController();
+
+  Future<void> read() async {
+    passwords = await _passwordDBController.read();
+    notifyListeners();
+  }
+
+  Future<bool> create(Passwords password) async {
+    int newRowId = await _passwordDBController.create(password);
+
+    if (newRowId != 0) {
+      password.id = newRowId;
+      passwords.add(password);
+      notifyListeners();
+    }
+
+    return newRowId != 0;
+  }
+
+  Future<bool> delete(int id) async {
+    bool deleted = await _passwordDBController.delete(id);
+    if(deleted){
+    
+      int index = passwords.indexWhere((element) => element.id == id);
+      if(index != -1){
+        passwords.removeAt(index);
+        notifyListeners();
+      }
+    }
+    return deleted;
+  }
+
+Future<bool> update(Passwords contact) async{
+    bool updated = await _passwordDBController.update(contact);
+    if(updated){
+      
+      int index = passwords.indexWhere((element) => element.id == contact.id);
+      if(index != -1) {
+        passwords[index] = contact;
+        notifyListeners();
+      }
+    }
+    return updated;
+  }
+
+}
